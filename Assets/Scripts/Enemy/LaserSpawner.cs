84beaeab83901cd6b338792aa54a8c393b5979bc
Edward Vaudin <edward@vaudin.net>
2022-08-23 21:36:20 +0100

    Laser Spawner now rotates to face player and fires

--- Assets/Scripts/Enemy/LaserSpawner.cs
+++ Assets/Scripts/Enemy/LaserSpawner.cs
@@ -7,9 +7,15 @@ public class LaserSpawner : MonoBehaviour
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float spawnRate = 1f;
     private float timeSinceLastFire = 0;
+    private Transform player;
 
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
     private void Update()
     {
+        transform.right = player.position - transform.position;
         timeSinceLastFire += Time.deltaTime;
         if (timeSinceLastFire > spawnRate)
         {

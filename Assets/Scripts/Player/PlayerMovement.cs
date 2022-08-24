d7e600519ea33a79b4e790a549d1c2f54c6a4ac0
Doc-Steel <93289194+Doc-Steel@users.noreply.github.com>
2022-08-24 22:15:32 +0100

    Update PlayerMovement.cs
    Allows Player to Jump with Left Click

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -55,7 +55,7 @@ public class PlayerMovement : MonoBehaviour
     {
         SetGroundedStatus();
         SetSpeed();
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) In && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
         }
